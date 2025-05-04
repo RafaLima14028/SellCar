5e220da8009f7a47f53af8f086e00908f75a121b
Rafa <rafael.alv334@gmail.com>
2025-05-04 07:02:25 -0300

    :rocket: chore: Implementação de filtragem na home-interna

--- home-interna/assets/scripts/scripts.js
+++ home-interna/assets/scripts/scripts.js
@@ -1,9 +1,9 @@
+var paginaAtual = 1;
+var carregando = false;
+
+const listaAnuncios = document.getElementById("lista-anuncios");
+
 document.addEventListener("DOMContentLoaded", async () => {
-  var paginaAtual = 1;
-  var carregando = false;
-
-  const listaAnuncios = document.getElementById("lista-anuncios");
-
   async function verificaSeEstaLogado() {
     try {
       let response = await fetch(
@@ -28,115 +28,6 @@ document.addEventListener("DOMContentLoaded", async () => {
       window.location.href = redirecionamento_url;
     }
   }
-
-  function montaCard(img0, marca, modelo, ano, cidade, valor) {
-    let sectionContainerCard = document.createElement("section");
-    let divcontainerImg = document.createElement("div");
-    let divDetalhesCarro = document.createElement("div");
-    let divDetalhesCarroEsquerdo = document.createElement("div");
-    let divDetalhesCarroDireito = document.createElement("div");
-    let divDetalhesCarroDireitoCidade = document.createElement("div");
-    let divDetalhesCarroValor = document.createElement("div");
-
-    sectionContainerCard.className = "container-card";
-    divcontainerImg.className = "container-img";
-    divDetalhesCarro.className = "detalhes-carro";
-    divDetalhesCarroEsquerdo.className = "detalhes-carro-esquerda";
-    divDetalhesCarroDireito.className = "detalhes-carro-direto";
-    divDetalhesCarroDireitoCidade.className = "detalhes-carro-direto-cidade";
-    divDetalhesCarroValor.className = "detalhes-carro-valor";
-
-    let caminhoFoto = "../../app/" + img0;
-    let img = document.createElement("img");
-    img.src = caminhoFoto;
-    img.alt = "Imagem do carro";
-
-    let aRedireciona = document.createElement("a");
-    aRedireciona.href = "../detalhes-anuncio/index.html";
-
-    divcontainerImg.appendChild(img);
-    aRedireciona.appendChild(divcontainerImg);
-    sectionContainerCard.appendChild(aRedireciona);
-
-    let pMarca = document.createElement("p");
-    pMarca.innerText = "Marca: " + marca;
-    let pModelo = document.createElement("p");
-    pModelo.innerText = "Modelo: " + modelo;
-    let pAno = document.createElement("p");
-    pAno.innerText = "Ano: " + ano;
-
-    divDetalhesCarroEsquerdo.appendChild(pMarca);
-    divDetalhesCarroEsquerdo.appendChild(pModelo);
-    divDetalhesCarroEsquerdo.appendChild(pAno);
-    divDetalhesCarro.appendChild(divDetalhesCarroEsquerdo);
-
-    let pCidade = document.createElement("p");
-    pCidade.innerText = "Em " + cidade;
-
-    divDetalhesCarroDireitoCidade.appendChild(pCidade);
-    divDetalhesCarroDireito.appendChild(divDetalhesCarroDireitoCidade);
-    divDetalhesCarro.appendChild(divDetalhesCarroDireito);
-    sectionContainerCard.appendChild(divDetalhesCarro);
-
-    let pValor = document.createElement("p");
-    pValor.innerHTML = "<strong>R$ " + valor + "</strong>";
-
-    divDetalhesCarroValor.appendChild(pValor);
-    sectionContainerCard.appendChild(divDetalhesCarroValor);
-
-    listaAnuncios.appendChild(sectionContainerCard);
-  }
-
-  async function exibirAnuncios(anuncios) {
-    anuncios.forEach((anuncio) => {
-      let marca = anuncio.marca;
-      let modelo = anuncio.modelo;
-      let ano = anuncio.ano;
-      let cidade = anuncio.cidade;
-      let valor = anuncio.valor;
-      let img0 = anuncio.fotos[0];
-
-      montaCard(img0, marca, modelo, ano, cidade, valor);
-    });
-  }
-
-  async function carregarAnuncios() {
-    if (carregando) return;
-
-    carregando = true;
-
-    try {
-      let response = await fetch(
-        "../../app/controlador.php?acao=listarAnuncios&pagina=" + paginaAtual
-      );
-
-      if (!response.ok) {
-        alert("Erro ao carregar os anúncios");
-        return;
-      }
-
-      let anuncios = await response.json();
-
-      if (anuncios.length === 0) return;
-
-      paginaAtual++;
-
-      exibirAnuncios(anuncios);
-    } catch (error) {
-      alert("Erro ao carregar os anúncios");
-    } finally {
-      carregando = false;
-    }
-  }
-
-  window.addEventListener("scroll", () => {
-    if (
-      window.innerHeight + window.screenY >=
-      document.body.offsetHeight - 100
-    ) {
-      carregarAnuncios();
-    }
-  });
 
   await verificaSeEstaLogado();
   carregarAnuncios();
@@ -180,3 +71,258 @@ a_logoff.onclick = async () => {
   alert("Logoff realizado com sucesso!");
   window.location.href = "../../../index.html";
 };
+
+function montaCard(img0, marca, modelo, ano, cidade, valor) {
+  let sectionContainerCard = document.createElement("section");
+  let divcontainerImg = document.createElement("div");
+  let divDetalhesCarro = document.createElement("div");
+  let divDetalhesCarroEsquerdo = document.createElement("div");
+  let divDetalhesCarroDireito = document.createElement("div");
+  let divDetalhesCarroDireitoCidade = document.createElement("div");
+  let divDetalhesCarroValor = document.createElement("div");
+
+  sectionContainerCard.className = "container-card";
+  divcontainerImg.className = "container-img";
+  divDetalhesCarro.className = "detalhes-carro";
+  divDetalhesCarroEsquerdo.className = "detalhes-carro-esquerda";
+  divDetalhesCarroDireito.className = "detalhes-carro-direto";
+  divDetalhesCarroDireitoCidade.className = "detalhes-carro-direto-cidade";
+  divDetalhesCarroValor.className = "detalhes-carro-valor";
+
+  let caminhoFoto = "../../app/" + img0;
+  let img = document.createElement("img");
+  img.src = caminhoFoto;
+  img.alt = "Imagem do carro";
+
+  let aRedireciona = document.createElement("a");
+  aRedireciona.href = "../detalhes-anuncio/index.html";
+
+  divcontainerImg.appendChild(img);
+  aRedireciona.appendChild(divcontainerImg);
+  sectionContainerCard.appendChild(aRedireciona);
+
+  let pMarca = document.createElement("p");
+  pMarca.innerText = "Marca: " + marca;
+  let pModelo = document.createElement("p");
+  pModelo.innerText = "Modelo: " + modelo;
+  let pAno = document.createElement("p");
+  pAno.innerText = "Ano: " + ano;
+
+  divDetalhesCarroEsquerdo.appendChild(pMarca);
+  divDetalhesCarroEsquerdo.appendChild(pModelo);
+  divDetalhesCarroEsquerdo.appendChild(pAno);
+  divDetalhesCarro.appendChild(divDetalhesCarroEsquerdo);
+
+  let pCidade = document.createElement("p");
+  pCidade.innerText = "Em " + cidade;
+
+  divDetalhesCarroDireitoCidade.appendChild(pCidade);
+  divDetalhesCarroDireito.appendChild(divDetalhesCarroDireitoCidade);
+  divDetalhesCarro.appendChild(divDetalhesCarroDireito);
+  sectionContainerCard.appendChild(divDetalhesCarro);
+
+  let pValor = document.createElement("p");
+  pValor.innerHTML = "<strong>R$ " + valor + "</strong>";
+
+  divDetalhesCarroValor.appendChild(pValor);
+  sectionContainerCard.appendChild(divDetalhesCarroValor);
+
+  listaAnuncios.appendChild(sectionContainerCard);
+}
+
+async function exibirAnuncios(anuncios) {
+  anuncios.forEach((anuncio) => {
+    let marca = anuncio.marca;
+    let modelo = anuncio.modelo;
+    let ano = anuncio.ano;
+    let cidade = anuncio.cidade;
+    let valor = anuncio.valor;
+    let img0 = anuncio.fotos[0];
+
+    montaCard(img0, marca, modelo, ano, cidade, valor);
+  });
+}
+
+async function carregarAnuncios() {
+  if (carregando) return;
+
+  carregando = true;
+
+  try {
+    let response = await fetch(
+      "../../app/controlador.php?acao=listarAnuncios&pagina=" + paginaAtual
+    );
+
+    if (!response.ok) {
+      alert("Erro ao carregar os anúncios");
+      return;
+    }
+
+    let anuncios = await response.json();
+
+    if (anuncios.length === 0) return;
+
+    paginaAtual++;
+
+    exibirAnuncios(anuncios);
+  } catch (error) {
+    alert("Erro ao carregar os anúncios");
+  } finally {
+    carregando = false;
+  }
+}
+
+window.addEventListener("scroll", () => {
+  if (window.innerHeight + window.screenY >= document.body.offsetHeight - 100) {
+    carregarAnuncios();
+  }
+});
+
+async function carregaMarcasDistintas() {
+  let marca = document.getElementById("marca");
+
+  let response = await fetch("../../app/controlador.php?acao=marcasDistintas");
+
+  let dados = await response.json();
+
+  if (!response.ok) {
+    alert(dados.message);
+    return;
+  }
+
+  let option = document.createElement("option");
+  option.value = "";
+  option.textContent = "";
+
+  marca.appendChild(option);
+
+  dados.forEach((marcaItem) => {
+    let option = document.createElement("option");
+    option.value = marcaItem;
+    option.textContent = marcaItem;
+
+    marca.appendChild(option);
+  });
+}
+
+async function carregaModelosDaMarcaDistintos() {
+  let marca = document.getElementById("marca");
+
+  if (marca.value === "") return;
+
+  let response = await fetch(
+    "../../app/controlador.php?acao=modelosDestintas&marca=" + marca.value
+  );
+
+  let dados = await response.json();
+
+  if (!response.ok) {
+    alert(dados.message);
+    return;
+  }
+
+  let option = document.createElement("option");
+  option.value = "";
+  option.textContent = "";
+
+  modeloSelect.appendChild(option);
+
+  dados.forEach((marcaItem) => {
+    let option = document.createElement("option");
+    option.value = marcaItem;
+    option.textContent = marcaItem;
+
+    modeloSelect.appendChild(option);
+  });
+}
+
+async function carregaCidades() {
+  let marca = document.getElementById("marca");
+  let modelo = document.getElementById("modelo");
+
+  if (marca.value === "" || modelo.value === "") return;
+
+  let response = await fetch(
+    "../../app/controlador.php?acao=cidadesDestintas&marca=" +
+      marca.value +
+      "&modelo=" +
+      modelo.value
+  );
+
+  let dados = await response.json();
+
+  if (!response.ok) {
+    alert(dados.message);
+    return;
+  }
+
+  let option = document.createElement("option");
+  option.value = "";
+  option.textContent = "";
+
+  cidadeSelect.appendChild(option);
+
+  dados.forEach((marcaItem) => {
+    let option = document.createElement("option");
+    option.value = marcaItem;
+    option.textContent = marcaItem;
+
+    cidadeSelect.appendChild(option);
+  });
+}
+
+async function carregaVeiculosDoModelo() {
+  if (marca.value === "" || modelo.value === "" || cidade.value === "") {
+    return;
+  }
+
+  let response = await fetch(
+    "../../app/controlador.php?acao=filtrarAnuncios&marca=" +
+      marca.value +
+      "&modelo=" +
+      modelo.value +
+      "&cidade=" +
+      cidade.value
+  );
+
+  let dados = await response.json();
+
+  if (!response.ok) {
+    alert(dados.message);
+    return;
+  }
+
+  funcaoAtualDeAtualizacaoDoScroll = carregaVeiculosDoModelo;
+
+  listaAnuncios.innerHTML = "";
+
+  dados.forEach((anuncio) => {
+    let marca = anuncio.marca;
+    let modelo = anuncio.modelo;
+    let ano = anuncio.ano;
+    let cidade = anuncio.cidade;
+    let valor = anuncio.valor;
+    let img0 = anuncio.fotos[0];
+
+    montaCard(img0, marca, modelo, ano, cidade, valor);
+  });
+}
+
+window.onload = () => carregaMarcasDistintas();
+
+const modeloSelect = document.getElementById("modelo");
+modeloSelect.onchange = () => {
+  cidadeSelect.innerHTML = "";
+  carregaCidades();
+};
+const cidadeSelect = document.getElementById("cidade");
+
+const marcaSelect = document.getElementById("marca");
+marcaSelect.onchange = () => {
+  modeloSelect.innerHTML = "";
+  cidadeSelect.innerHTML = "";
+  carregaModelosDaMarcaDistintos();
+};
+
+const btnBuscarCarro = document.getElementById("buscarCarro");
+btnBuscarCarro.onclick = () => carregaVeiculosDoModelo();
